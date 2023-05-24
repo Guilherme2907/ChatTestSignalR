1df215382a4f448234abfb3a15ce38b4858c84d0
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2023-05-23 22:14:38 -0300

    Creation of a blazorserver containing a hub (ChatHub) and a razor page (Index.razor) for communication via websocket using the signalr library

---
+++ BlazorServer/Hubs/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorServer.Hubs;
+
+public class ChatHub : Hub
+{
+    private static Dictionary<string, string> users = new ();
+
+    public Task SendMessage(string sender, string message, string recipient)
+    {
+        var connectionIdRecipient= users[recipient];
+        var connectionIdSender = users[sender];
+        return Clients.Clients(connectionIdRecipient, connectionIdSender).SendAsync("ReceiveMessage", sender, message);
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        string userId = Context.GetHttpContext().Request.Query["user"];
+
+        users[userId] = Context.ConnectionId;
+
+        return base.OnConnectedAsync();
+    }
+}
